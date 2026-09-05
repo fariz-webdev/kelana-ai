9b8de8efd228a33ecda603c53d656d9e2699f1c0
fariz-webdev <fariz.webdeveloper@gmail.com>
2026-09-05 22:10:53 +0700

    Edit fetch data at homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
     //   });
       try {
       const token = localStorage.getItem("access_token");
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/trips`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/trips`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
